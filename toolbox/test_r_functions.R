5fd6ec46be072c969bad3fd878ee152b7bde2e4c
Victoria Cheung <vcheung@revmed.com>
2025-06-12 14:44:32 -0700

    fix string errors

--- toolbox/test_r_functions.R
+++ toolbox/test_r_functions.R
@@ -1,18 +1,12 @@
-"""
-This script here contains functions just to test rpy2 sanity checking 
-"""
-# %%
-# Here is a function to do addition to test rpy2
+# This script contains functions just to test rpy2 sanity checking
+
 my_addition_function <- function(a, b) {
   result <- a + b
   result
 }
 
-# %%
-# Here is a function that has some dataframe stuff
 my_data_processing_function <- function(input_df) {
-  # Perform some operations on the input data frame
   output_df <- input_df
-  output_df$new_column <- rowMeans(input_df)  # Example: calculate row means
+  output_df$new_column <- rowMeans(input_df)
   output_df
 }
