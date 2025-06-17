6283a81bc5df1f504c21479dfc5f5be961244c6c
Victoria Cheung <vcheung@revmed.com>
2025-06-16 19:39:37 -0700

    add dataframe function test

--- tm_vctoolbox/rpy2_scratchpad/test_r_functions.R
+++ tm_vctoolbox/rpy2_scratchpad/test_r_functions.R
@@ -10,3 +10,7 @@ my_data_processing_function <- function(input_df) {
   output_df$new_column <- rowMeans(input_df)
   output_df
 }
+
+my_df_func <- function(n) {
+  data.frame(x = 1:n, y = rnorm(n))
+}
